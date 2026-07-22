7ca74dbb8b6ee0740730f55e5986b136241a5876
Foxyoo5 <NMFox005@Gmail.com>
2026-07-22 10:17:37 +0100

    Create fetch_movies.py

---
+++ fetch_movies.py
@@ -0,0 +1,54 @@
+import requests
+import re
+import os
+import datetime
+
+current_year = datetime.datetime.utcnow().year
+
+url = (
+    f"https://bingebase.com/movies/new-releases"
+    f"?sort=recent&year_from={current_year}&year_to="
+    f"&country%5B%5D=US&country%5B%5D=GB&language%5B%5D=en"
+)
+
+headers = {"User-Agent": "Mozilla/5.0 (compatible; RSSFeedBot/1.0)"}
+resp = requests.get(url, headers=headers, timeout=30)
+html = resp.text
+
+# Match each poster image + link + following "rating Title (year)" heading
+pattern = re.compile(
+    r'<img[^>]+alt="([^"]+?) poster"[^>]+src="(https://cdn\.bingebase\.com/[^"]+)"[^>]*>'
+    r'.*?href="(https://bingebase\.com/movies/[^"]+)"'
+    r'.*?<h3[^>]*>\s*([\d.]+)\s+.*?\((\d{4})\)',
+    re.DOTALL
+)
+
+matches = pattern.findall(html)[:20]
+
+q = chr(34)
+items_xml = ""
+
+for title, poster, link, rating, year in matches:
+    title_clean = title.replace("&", "&amp;")
+    items_xml += "<item>"
+    items_xml += f"<title>{title_clean}</title>"
+    items_xml += "<description>" + chr(60) + "![CDATA[" + \
+        f'<img src=' + q + poster + q + f'><br>Rating: {rating}' + \
+        "]]" + chr(62) + "</description>"
+    items_xml += f"<link>{link}</link>"
+    items_xml += "</item>"
+
+rss = '<?xml version=' + q + '1.0' + q + ' encoding=' + q + 'UTF-8' + q + '?>'
+rss += "<rss version=" + q + "2.0" + q + ">"
+rss += "<channel>"
+rss += "<title>New Movie Releases</title>"
+rss += "<link>https://foxyoo5.github.io/New-Movie-Releases/new-movie-releases.xml</link>"
+rss += "<description>Latest movie releases from BingeBase</description>"
+rss += items_xml
+rss += "</channel></rss>"
+
+os.makedirs("docs", exist_ok=True)
+with open("docs/new-movie-releases.xml", "w", encoding="utf-8") as f:
+    f.write(rss)
+
+print(f"Found {len(matches)} movies")
